cb5a1b71e0c1d661a37cee04fcc5a1361c5caf9d
umaj2ee <ulakkaraju@gmail.com>
2018-03-05 12:39:59 -0800

    first commit

---
+++ src/leet/Combination.java
@@ -0,0 +1,20 @@
+package leet;
+import java.util.Arrays;
+
+public class Combination {
+    public static void main(String[] args){
+        String[] arr = {"H","E","L","O","U","M"};
+        combinations2(arr, 3, 0, new String[3]);
+    }
+
+    static void combinations2(String[] arr, int len, int startPosition, String[] result){
+        if (len == 0){
+            System.out.println(Arrays.toString(result));
+            return;
+        }       
+        for (int i = startPosition; i <= arr.length-len; i++){
+            result[result.length - len] = arr[i];
+            combinations2(arr, len-1, i+1, result);
+        }
+    }       
+}
